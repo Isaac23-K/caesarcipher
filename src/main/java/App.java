6cc71db253cfae7390383acbfd154c5128736cff
Isaac23-K <ikaptoge@yahoo.com>
2021-02-15 01:32:50 +0300

    Added a shift key code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,6 +10,19 @@ public class App {
 
 
         String choice = option.nextLine();
+
+        // enter text
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a word ");
+        String plainText = input.nextLine();
+        System.out.println("Original plain text : " + plainText);
+
+        // enter shift key or number
+        Scanner inputKey = new Scanner(System.in);
+        System.out.println("Your shift key is : " );
+        int key = Integer.parseInt(inputKey.nextLine());
+        System.out.println("Your shift key is : " + key );
+
     }
     }
 }
