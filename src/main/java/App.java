1986c593310eb0a3f06b466ad2f96772dac8ec5a
Isaac23-K <ikaptoge@yahoo.com>
2021-02-15 01:25:13 +0300

    inital commit

---
+++ src/main/java/App.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+    boolean theAppIsRunning = true;
+    while (theAppIsRunning){
+        // user to pick option
+        Scanner option = new Scanner(System.in);
+        System.out.println("Welcome to Ceaser Cipher App : Encode , Decode or Leave");
+
+
+        String choice = option.nextLine();
+    }
+    }
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,10 @@
+import org.junit.*;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    public void runLetter_enCodeLetter_shiftLetter(){
+       int shit = 1;
+
+    }
+}
