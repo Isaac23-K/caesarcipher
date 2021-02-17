38dc8504ddfa1b050f94cf32808af67fdb5d31e5
Isaac23-K <60667301+Isaac23-K@users.noreply.github.com>
2021-02-17 08:57:08 +0300

    Update AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,16 +1,19 @@
-import org.junit.jupiter.api.Test;
+port org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
 
+    // test input to be string
     @Test
     public void  testEncoding_testWord_String(){
         Encode testEncode = new Encode();
-
+        assertEquals(true , testEncode instanceof Encode);
     }
 
     @Test
     public void decodeTest_testWord_String(){
 
         Decode decodeTest = new Decode();
+        assertEquals(true, decodeTest instanceof Decode);
     }
 }
